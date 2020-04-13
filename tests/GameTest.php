fb422cd3e9a2714540d8b695e855f6b33470fa51
Aldrin Papa <apapa@cambridge.org>
2020-04-13 10:54:50 +0800

    refactor

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -5,21 +5,24 @@ use Mini\Player;
 
 class GameTest extends PHPUnit_Framework_TestCase
 {
+    protected $game;
+
+    public function setUp()
+    {
+        $this->game = new Game();
+        $player1 = new Player();
+        $this->game->addPlayer($player1);
+    }
+
     public function testPlayerShouldStartOnFirstBlock()
     {
-        $game = new Game();
-        $player1 = new Player();
-        $game->addPlayer($player1);
-        $player = $game->getPlayer(1);
+        $player = $this->game->getPlayer(1);
         $this->assertEquals(1, $player->getPosition());
     }
 
     public function testPlayerShouldBeInFifthBlockAfterMovingFourBlock()
     {
-        $game = new Game();
-        $player1 = new Player();
-        $game->addPlayer($player1);
-        $player = $game->getPlayer(1);
+        $player = $this->game->getPlayer(1);
         $player->move(4);
         $this->assertEquals(5, $player->getPosition());
     }
