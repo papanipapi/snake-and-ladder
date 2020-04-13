1b322a6b1c342e84b279199fb88d10af65c1444b
Aldrin Papa <apapa@cambridge.org>
2020-04-13 10:53:08 +0800

    add test player moving

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -13,4 +13,14 @@ class GameTest extends PHPUnit_Framework_TestCase
         $player = $game->getPlayer(1);
         $this->assertEquals(1, $player->getPosition());
     }
+
+    public function testPlayerShouldBeInFifthBlockAfterMovingFourBlock()
+    {
+        $game = new Game();
+        $player1 = new Player();
+        $game->addPlayer($player1);
+        $player = $game->getPlayer(1);
+        $player->move(4);
+        $this->assertEquals(5, $player->getPosition());
+    }
 }
